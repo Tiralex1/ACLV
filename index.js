1780f202e80d179d28aaba56cf2fa0eec27ec429
Alexis Launay <tiralex.gamer@gmail.com>
2022-10-22 22:59:07 +0200

    update affichage base de données JSON

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ function data(nom_fic)
     $.getJSON(nom_fic, function (data) {
         var items = [];
         $.each(data, function (key, val) {
-            items.push("<tr id=" + key + "><td>" + val + "</td></tr>");
+            items.push("<tr id='" + key + "'>");
+            $.each(val, function (key, val2) {
+                items.push("<td id='" + key + "'>" + val2 + "</td>");
+            });
+            items.push("</tr>");
         });
 
         $("<table/>", {

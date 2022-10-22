b221986722d395a00ca81e3ce9906a30cd5f8c5e
Alexis Launay <tiralex.gamer@gmail.com>
2022-10-22 22:42:24 +0200

    ajout td

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function data(nom_fic)
     $.getJSON(nom_fic, function (data) {
         var items = [];
         $.each(data, function (key, val) {
-            items.push("<tr id='" + key + "'>" + val + "</tr>");
+            items.push("<tr id='" + key + "'><td>" + val + "</td></tr>");
         });
 
         $("<table/>", {

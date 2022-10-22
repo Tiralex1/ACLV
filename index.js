04702a1bffc306ea41b11e9461655df6abb10d22
Alexis Launay <tiralex.gamer@gmail.com>
2022-10-23 00:10:36 +0200

    debug

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ function data(nom_fic)
     $.getJSON(nom_fic, function (data) {
         var items = [];
         $.each(data, function (key, val) {
-            $.each(val, function (key, val2) {
+            $.each(val, function (key, val) {
                 items.push("<tr id='" + key + "'>");
-                var j = 0;
-                while (val2.musique[j] != null) {
-                    items.push("<td id='nom_anime'>" + val2.nom + "</td>");
-                    items.push("<td id='type'>" + val2.musique[j].type + "</td>");
-                    items.push("<td id='numero'>" + val2.musique[j].numero + "</td>");
-                    items.push("<td id='nom'>" + val2.musique[j].nom + "</td>");
-                    items.push("<td id='artiste'>" + val2.musique[j].artiste + "</td>");
-                    j++;
-                }
+                $.each(val, function (key, val) {
+                    if (key == "musique") {
+                        items.push("<td>" + val[j].type + "</td>");
+                    }
+                    else items.push("<td>" + val + "</td>");
+                });
                 items.push("</tr>");
             });
         });

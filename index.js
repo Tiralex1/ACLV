4f87e8af7dbe5d589e39d1c457b2be0ce367671b
Alexis Launay <tiralex.gamer@gmail.com>
2022-10-22 23:36:13 +0200

    affichage info present dans database

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ function data(nom_fic)
         var items = [];
         $.each(data, function (key, val) {
             var i = 0;
-            items.push("<tr id='" + key + "'>");
-            while (val[i] != null) { items.push("<td>" + val[i].nom + "</td>"); i++; }
-            items.push("</tr>");
+            while (val[i] != null) {
+                items.push("<tr id='" + i + "'>");
+                var j = 0;
+                while (val[i].musique[j] != null) { 
+                    items.push("<td id='nom_anime'>" + val[i].nom + "</td>");
+                    items.push("<td id='type'>" + val[i].musique[j].type + "</td>");
+                    items.push("<td id='numero'>" + val[i].musique[j].numero + "</td>");
+                    items.push("<td id='nom'>" + val[i].musique[j].nom + "</td>");
+                    items.push("<td id='artiste'>" + val[i].musique[j].artiste + "</td>");
+                    j++;
+                }
+                items.push("</tr>");
+                i++;
+            }
         });
 
         $("<table/>", {

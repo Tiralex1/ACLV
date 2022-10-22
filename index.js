37a7bafd81c0476239b884508a9597bf631257d2
Alexis Launay <tiralex.gamer@gmail.com>
2022-10-23 00:46:22 +0200

    while infini debug

--- index.js
+++ index.js
@@ -4,18 +4,14 @@ function data(nom_fic)
         var items = [];
         $.each(data, function (key, val) {
             $.each(val, function (key, val) {
-                var i = val.nb_musique;
-                while (i != -1) {
+                for (var i = 0; i < val.nb_musique; ++i) {
                     items.push("<tr id='" + key + "'>");
                     $.each(val, function (key, val) {
                         if (key == "musique") {
-                            if (val[i] == null) i = -1;
-                            else {
-                                items.push("<td>" + val[i].type + "</td>");
-                                items.push("<td>" + val[i].numero + "</td>");
-                                items.push("<td>" + val[i].nom + "</td>");
-                                items.push("<td>" + val[i].artiste + "</td>");
-                            }
+                            items.push("<td>" + val[i].type + "</td>");
+                            items.push("<td>" + val[i].numero + "</td>");
+                            items.push("<td>" + val[i].nom + "</td>");
+                            items.push("<td>" + val[i].artiste + "</td>");
                         }
                         else items.push("<td>" + val + "</td>");
                     });

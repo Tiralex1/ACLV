c7345f18c8f29af3576cdeb36c49d254cf099bfa
Alexis Launay <tiralex.gamer@gmail.com>
2022-11-03 17:47:05 +0100

    test

--- index.js
+++ index.js
@@ -21,23 +21,8 @@ const Red = "red";
 const Green = "green";
 
 function correctString(str) {
-    var string = str.split("<");
-    var string2 = [];
-    for (var i = 0; i < string.length-1; ++i) {
-        string2.push(string[i]);
-        string2.push("\\" + "<");
-        console.log(string2);
-    }
-    string2.push(string[string.length - 1]);
-    string = string2.join("");
-    string = string.split(">");
-    string2 = [];
-    for (var i = 0; i < string.length-1; ++i) {
-        string2.push(string[i]);
-        string2.push("\\" + ">");
-    }
-    string2.push(string[string.length - 1]);
-    string = string2.join("");
+    var string = str.replace("<", "\<");
+    string = string.replace(">", "\>");
     return string;
 }
 

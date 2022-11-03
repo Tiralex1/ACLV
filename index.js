2b4f2a82f6e8e7313bc064956fe8c6437f9d6bb4
Alexis Launay <tiralex.gamer@gmail.com>
2022-11-03 17:48:55 +0100

    test

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const Red = "red";
 const Green = "green";
 
 function correctString(str) {
-    var string = str.replace("<", "\<");
-    string = string.replace(">", "\>");
+    var string = str.replace("<", "\\'<");
+    string = string.replace(">", "\\'>");
     return string;
 }
 

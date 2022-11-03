7ded2b633c91f235b3600ccf8ef00a664007bf5f
Alexis Launay <tiralex.gamer@gmail.com>
2022-11-03 18:25:30 +0100

    test

--- index.js
+++ index.js
@@ -25,19 +25,18 @@ function correctString(str) {
     var string2 = [];
     for (var i = 0; i < string.length-1; ++i) {
         string2.push(string[i]);
-        string2.push("\\<");
-        console.log(string2);
+        string2.push("\<");
     }
-    string2.push(string[string.length - 1]);
-    string = string2.join("");
+    if (string.length - 1 > 0) string = string2.join("");
+    else string = string[0];
     string = string.split(">");
     string2 = [];
     for (var i = 0; i < string.length-1; ++i) {
         string2.push(string[i]);
-        string2.push("\\" + ">");
+        string2.push("\>");
     }
-    string2.push(string[string.length - 1]);
-    string = string2.join("");
+    if (string.length - 1 > 0) string = string2.join("");
+    else string = string[0];
     return string;
 }
 

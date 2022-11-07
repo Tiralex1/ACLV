faa6d1548a1c6739dfcaa6acd490d1588361327d
Alexis Launay <tiralex.gamer@gmail.com>
2022-11-07 18:01:39 +0100

    index.js

--- index.js
+++ index.js
@@ -175,10 +175,8 @@ function affich(tab_anime, tab_musique) {
         }
         for (var j = tab_anime[i][3]; j < tab_anime[i][2] + tab_anime[i][3]; ++j) {
             items.push("<tr class=" + bg + ">");
-            if (affich_fav == true) {
-                if (tab_musique[j][5] == false) items.push("<td class='favo' onclick='ajout_fav(" + ind + ");'>&#10025;</td>");
-                else items.push("<td class='favo' onclick='ajout_fav(" + ind + ");'>&#9733;</td>");
-            }
+            if (tab_musique[j][5] == false) items.push("<td class='favo' onclick='ajout_fav(" + ind + ");'>&#10025;</td>");
+            else items.push("<td class='favo' onclick='ajout_fav(" + ind + ");'>&#9733;</td>");
             items.push("<td class='noman'>" + tab_anime[i][0] + "</td>");
             items.push("<td class='typm'>" + tab_musique[j][0] + "</td>");
             if (tab_musique[j][1] != 0) items.push("<td class='num'>" + tab_musique[j][1] + "</td>");
